b99b31b2cac09c42f298f495e874a3ef93b6f2b8
Ivanov Pavel Gennadievich <ivanov.pavel.gennadievich@gmail.com>
2021-03-24 09:34:22 +0300

    First commit.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace KeyboardHook
+{
+	static class Program
+	{
+		[STAThread]
+		static void Main()
+		{
+			foreach (var filter in USBPcapLib.USBPcapClient.find_usbpcap_filters())
+			{
+				Console.WriteLine(filter);
+				Console.WriteLine(USBPcapLib.USBPcapClient.enumerate_print_usbpcap_interactive(filter));
+			}
+			var usbClient = new USBPcapLib.USBPcapClient(@"\\.\USBPcap2", 3);
+			usbClient.DataRead += OnDataRead;
+			usbClient.start_capture();
+			while (true)
+			{
+				Thread.Sleep(1);
+			}
+		}
+
+		private static void OnDataRead(object sender, USBPcapLib.DataEventArgs e)
+		{
+			Console.WriteLine(BitConverter.ToString(e.Data));
+			if ((e.Data.Length >= 3) && (e.Data[2] != 0))
+			{
+				Console.WriteLine(e.Data[2]);
+			}
+		}
+	}
+}
